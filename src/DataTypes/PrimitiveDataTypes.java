ab847082d00da5099b7dd05f633faa3461639429
Yerfdog1990 <gouma308@gmail.com>
2024-06-19 05:21:00 +0700

    Data types basics

---
+++ src/DataTypes/PrimitiveDataTypes.java
@@ -0,0 +1,15 @@
+package DataTypes;
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        System.out.println(String.format("Maximum integer value: %s. Minimum integer value: %s.", Integer.MAX_VALUE, Integer.MIN_VALUE));
+        System.out.println(String.format("Maximum short value: %s. Minimum short value: %s.", Short.MAX_VALUE, Short.MIN_VALUE));
+        System.out.println(String.format("Maximum byte value: %s. Minimum byte value: %s.", Byte.MAX_VALUE, Byte.MIN_VALUE));
+        System.out.println(String.format("Maximum long value: %s. Minimum long value: %s.", Long.MAX_VALUE, Long.MIN_VALUE));
+        System.out.println(String.format("Maximum double value: %s. Minimum double value: %s.", Double.MAX_VALUE, Double.MIN_VALUE));
+        System.out.println(String.format("Maximum float value: %s. Minimum float value: %s.", Float.MAX_VALUE, Float.MIN_VALUE));
+        System.out.println(String.format("Maximum character value: %s. Minimum character value: %s.", Character.MAX_VALUE, Character.MIN_VALUE));
+        System.out.println(String.format("Boolean values: true or false."));
+        
+    }
+}
