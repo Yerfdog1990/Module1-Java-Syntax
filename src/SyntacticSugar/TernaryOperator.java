c26ac975dafdfda4c5f3108c3d3f93d137155b1c
Yerfdog1990 <gouma308@gmail.com>
2024-07-07 15:04:25 +0700

    Use ternary operator to demostrate syntactic sugar

--- src/SyntacticSugar/TernaryOperator.java
+++ src/SyntacticSugar/TernaryOperator.java
@@ -1,4 +1,17 @@
 package SyntacticSugar;
 
 public class TernaryOperator {
+    public static void main(String[] args) {
+        int age = 24;
+
+        //if-else condition
+        if(age >= 18){
+            System.out.println("You are eligible voter.");
+        }else{
+            System.out.println("You are not eligible to vote.");
+        }
+        //Ternary operator
+        String eligibility = (age >= 18)? "You are eligible voter." : "You are not eligible to vote.";
+        System.out.println(eligibility);
+    }
 }
