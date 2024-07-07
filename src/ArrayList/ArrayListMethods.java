4c774194f704bd25b5e3142ceb2ad658e15e39da
Yerfdog1990 <gouma308@gmail.com>
2024-07-07 12:42:24 +0700

    Array list methods demo

---
+++ src/ArrayList/ArrayListMethods.java
@@ -0,0 +1,22 @@
+package ArrayList;
+/*
+1.void add(E element): Adds the specified element to the end of the list.
+2.void add(int index, E element): Inserts the specified element at the specified position in the list.
+3.E get(int index): Returns the element at the specified position in the list.
+4.E set(int index, E element): Replaces the element at the specified position in the list with the specified element.
+5.E remove(int index): Removes the element at the specified position in the list and returns the removed element.
+6.boolean remove(Object o): Removes the first occurrence of the specified element from the list, if it is present.
+7.void clear(): Removes all elements from the list.
+8.boolean contains(Object o): Returns true if the list contains the specified element.
+9.int indexOf(Object o): Returns the index of the first occurrence of the specified element in the list, or -1 if the list does not contain the element.
+10.int lastIndexOf(Object o): Returns the index of the last occurrence of the specified element in the list, or -1 if the list does not contain the element.
+11.int size(): Returns the number of elements in the list.
+12.boolean isEmpty(): Returns true if the list contains no elements.
+13.Object[] toArray(): Returns an array containing all of the elements in the list in proper sequence.
+
+These methods provide basic operations to manipulate and access elements within an ArrayList in Java.
+ */
+
+public class ArrayListMethods {
+
+}
