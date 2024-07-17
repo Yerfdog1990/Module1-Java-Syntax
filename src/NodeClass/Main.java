05094aeffeeefc939e0f69e69ff92ee8410f46f5
Yerfdog1990 <gouma308@gmail.com>
2024-07-17 06:25:12 +0300

    Node Class for a Linked List

--- src/NodeClass/Main.java
+++ src/NodeClass/Main.java
@@ -6,11 +6,15 @@ public class Main {
         SinglyLinkedList list = new SinglyLinkedList<>();//Create a new empty list
         list.addFirst(10);//Add 10 at the beginning
         list.addFirst(20);//Add 20 at the beginning
+        list.addElement(25); //Add 25 at the middle
         list.addLast(30);//Add 30 at the end
         list.addLast(40);//Add 40 at the end
+        list.display(); //Display the list elements before removal
+        list.remove(50);    // Remove 20 from the list
+        list.display();      // Display the list elements after removal
 
-        //Display the list elements
-        list.display();
+
+
     }
 
 }
