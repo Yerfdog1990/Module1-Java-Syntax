3e4f1007cbe74686025507328b780070a78bc57c
Yerfdog1990 <gouma308@gmail.com>
2024-07-23 11:52:13 +0300

    Method 1 to convert the DayEnum to class

--- src/Enum/DayClass1.java
+++ src/Enum/DayClass1.java
@@ -28,30 +28,30 @@ public class DayClass1 {
         ALL_DAYS.add(SUNDAY);
     }
     //Field to store the name and ordinal of teh day
-    private final String name;
-    private final int ordinal;
+    private final String dayName;
+    private final int dayNumber;
 
     //Private constructor to initialize the day
-    private DayClass1(String name, int ordinal) {
-        this.name = name;
-        this.ordinal = ordinal;
+    private DayClass1(String dayName, int ordinal) {
+        this.dayName = dayName;
+        this.dayNumber = ordinal;
     }
     //Method to get all days
     public static List<DayClass1> getAllDays() {
         return ALL_DAYS;
     }
     //Method to get name of the day
-    public String getName() {
-        return name;
+    public String getDayName() {
+        return dayName;
     }
     //Method to get ordinal of the day
-    public int getOrdinal() {
-        return ordinal;
+    public int getDayNumber() {
+        return dayNumber;
     }
     //Method to get a day by its ordinal value
     public static DayClass1 findDayByOrdinal(int ordinal){
         for(DayClass1 day : ALL_DAYS){
-            if(day.getOrdinal() == ordinal){
+            if(day.getDayNumber() == ordinal){
                 return day;
             }
         }
@@ -60,7 +60,7 @@ public class DayClass1 {
     // Method to get a day by its name
     public static DayClass1 findDayByName(String name) {
         for (DayClass1 day : ALL_DAYS) {
-            if (day.getName().equalsIgnoreCase(name)) {
+            if (day.getDayName().equalsIgnoreCase(name)) {
                 return day;
             }
         }
@@ -77,21 +77,20 @@ public class DayClass1 {
     //Main method to demonstrate usage of DayClass1
     public static void main(String[] args) {
         //Print all days
-        //Print all days
         System.out.println("Days of the week and their ordinal:");
         for(DayClass1 day : DayClass1.ALL_DAYS){
-            System.out.println("Day: " +day.getName()+ " ,Ordinal: " +day.getOrdinal());
+            System.out.println("Day: " +day.getDayName()+ " ,Ordinal: " +day.getDayNumber());
         }
         //Check if Wednesday is a weekday or a weekend
         DayClass1 day = DayClass1.WEDNESDAY;
         System.out.println("Wednesday is a weekday: " +day.itIsWeekDay());
-        System.out.println("Wednesday is a weekend: " +day.itIsWeekDay());
+        System.out.println("Wednesday is a weekend: " +day.itIsWeekEnd());
 
         //Get day of the week by ordinal
         DayClass1 dayByOrdinal = DayClass1.findDayByOrdinal(1);
-        System.out.println("Day by ordinal 1: " +dayByOrdinal.getName());
+        System.out.println("Day by ordinal 1: " +dayByOrdinal.getDayName());
         //Get enum constant by name
         DayClass1 dayByName = DayClass1.findDayByName("TUESDAY");
-        System.out.println("Day by name: " +dayByName.getName());
+        System.out.println("Day by name: " +dayByName.getDayName());
     }
 }
