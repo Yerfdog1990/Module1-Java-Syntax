ea5776744ff965a31fef22a47778d1214ee3197b
Yerfdog1990 <gouma308@gmail.com>
2024-07-25 13:09:23 +0300

    FibonacciSeries using recursive method

--- src/Recursion/FibonacciSeries_RecursiveMethod.java
+++ src/Recursion/FibonacciSeries_RecursiveMethod.java
@@ -20,7 +20,7 @@ public class FibonacciSeries_RecursiveMethod {
     public static void main(String[] args) {
         int number = 10;
         for (int i = 0; i < number; i++) {
-            System.out.print(findFibonacciSeries(i) + ",");
+            System.out.print(findFibonacciSeries(i) + " ,");
         }
     }
 }
