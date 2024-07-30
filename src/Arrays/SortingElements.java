a20e8c2533ae7ef43b9adb2f74659a26c4a991fe
Yerfdog1990 <gouma308@gmail.com>
2024-07-30 06:22:56 +0300

    Sort of elements in an array.

--- src/Arrays/SortingElements.java
+++ src/Arrays/SortingElements.java
@@ -7,7 +7,7 @@ Write a Java program to sort a numeric array and a string array.
  */
 public class SortingElements {
     public static void main(String[] args) {
-        String[] arrayString = {"Godfrey", "Cyril", "Rosemary", "Cyrek"};
+        String[] arrayString = {"Godfrey", "Cyril", "Odhiambo", "Rosemary", "Cyrek"};
         int[] arrayNumber = {2, 5, 6, 1, 9, 8, 7};
         Arrays.sort(arrayNumber);
         Arrays.sort(arrayString);
