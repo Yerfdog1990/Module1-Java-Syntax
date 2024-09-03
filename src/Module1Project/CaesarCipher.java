e0481bb73b588a7385fdadfb7653f1e55ffc11f3
Yerfdog1990 <gouma308@gmail.com>
2024-09-04 05:16:42 +0700

    Added inputFile and outFile to CaesarCipher interface class

--- src/Module1Project/CaesarCipher.java
+++ src/Module1Project/CaesarCipher.java
@@ -1,6 +1,14 @@
 package Module1Project;
 
+import java.io.File;
+import java.io.IOException;
+
 public interface CaesarCipher {
-    String encrypt(String text, int key);
-    String decrypt(String text, int key);
+    // Encrypts the content of the input file and writes the encrypted content to an output file
+    void encrypt(File inputFile, File outputFile, int key) throws IOException;
+
+    // Decrypts the content of the input file and writes the decrypted content to an output file
+    void decrypt(File inputFile, File outputFile, int key) throws IOException;
 }
+
+
