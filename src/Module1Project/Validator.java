a411258090044f1249cce79cc91a770129b19a16
Yerfdog1990 <gouma308@gmail.com>
2024-09-07 12:01:25 +0700

    Completed reviewing the validator method.

--- src/Module1Project/Validator.java
+++ src/Module1Project/Validator.java
@@ -1,20 +1,16 @@
 package Module1Project;
 
-import java.io.*;
+import java.io.File;
 
 public class Validator {
-
     // Method to validate if the key is valid
     public boolean isValidKey(int key, char[] alphabet) {
         // A valid key should be between 0 and the length of the alphabet (inclusive of negative shifts)
         return key >= -alphabet.length && key <= alphabet.length;
     }
-
     // Method to check if a file exists at the given file path
     public boolean isFileExists(String filePath) {
         File file = new File(filePath);
         return file.exists() && file.isFile();
     }
 }
-
-
