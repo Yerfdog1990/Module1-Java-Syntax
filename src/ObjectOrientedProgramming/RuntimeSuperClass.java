3ffea67009440f03a5c9f6de03c421b88f470426
Yerfdog1990 <gouma308@gmail.com>
2024-09-14 08:10:08 +0700

    Removed implementation in the isKenyan() method.

--- src/ObjectOrientedProgramming/RuntimeSuperClass.java
+++ src/ObjectOrientedProgramming/RuntimeSuperClass.java
@@ -3,6 +3,5 @@ package ObjectOrientedProgramming;
 public class RuntimeSuperClass {
     //Super class method
     public void isKenyan(String name, String country){
-        System.out.println("My name is " +name+ " from " +country);
     }
 }
